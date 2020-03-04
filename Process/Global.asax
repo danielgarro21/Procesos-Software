64008b47fd2f835ff60593e064f2b97df9355f61
danielgarro21 <40309736+danielgarro21@users.noreply.github.com>
2020-03-03 18:00:55 -0600

    Add project files.

---
+++ Process/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Process.MvcApplication" Language="C#" %>
---
+++ Process/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Process.Startup))]
+namespace Process
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
